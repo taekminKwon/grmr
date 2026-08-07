342690be975a64f5e57bc1cbb03811675925226c
taekMinKwon <rnjsxorals1@gmail.com>
2026-08-07 14:38:29 +0900

    Settings : 프로젝트 시작

---
+++ grmr/src/main/java/com/ewha_eng/grmr/GrmrApplication.java
@@ -0,0 +1,13 @@
+package com.ewha_eng.grmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrmrApplication {
+
+        public static void main(String[] args) {
+                SpringApplication.run(GrmrApplication.class, args);
+        }
+
+}
---
+++ grmr/src/test/java/com/ewha_eng/grmr/GrmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewha_eng.grmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrmrApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+}
